751c396554cba583df4ed98945588d70c4ddc064
ITC - APP - Mobile Developer NGUYỄN VIỆT ANH <anh.nguyen28@mservice.com.vn>
2021-07-03 22:10:25 +0700

    update lib

--- index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Home from "./src/Home";
-
-function App() {
-  return <Home />;
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
